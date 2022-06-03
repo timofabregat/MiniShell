54b603cd0e38a1bb99fd52c5bbae283e2683424b
Juan Ignacio Ascione <janignacio.ascione@gmail.com>
2022-06-03 16:39:02 -0300

    Help hecho y funcionando

--- builtin_help.c
+++ builtin_help.c
@@ -2,11 +2,23 @@
 
 int builtin_help(int argc, char ** argv){
     struct builtin_struct *builtin = NULL;
-    builtin = builtin_lookup(argv[0]);
-    if (builtin == NULL){
-        printf("Comando no existe en libreria.\n");
+    if (argc == 1){
+        for (builtin = builtin_arr; builtin->cmd != NULL; builtin++)
+        {
+            printf("%s\n",builtin->help_txt);
+        }
+        
+    }
+    else if(argc == 2){
+        builtin = builtin_lookup(argv[1]);
+        if (builtin == NULL){
+            printf("Comando no existe en libreria.\n");
+        }
+        else{
+            printf("%s\n",builtin->help_txt);
+        }
     }
     else{
-        printf("%s",builtin->help_txt);
+        //COMPLETAR
     }
 }
--- mainshell.c
+++ mainshell.c
@@ -44,47 +44,47 @@ main(int argc, char *argv[]) {
                 continue;   // not EOF, read system call was interrupted, continue loop
             }
         }
-
-        if(linea2argv(line, MAXLINE, argv2) > 0){
+        argc = linea2argv(line, MAXLINE, argv2);
+        if(argc > 0){
             pid_t pid;                          // process ID: an unsigned integer type
             int wait_status;                    // wait status will be filled by waitpid syscall
+            ejecutar(argc,argv2);
+            // fprintf(stderr, "Will execute command %s\n",argv2[0]);
+            // fprintf(stderr, "Will fork command %s\n", argv2[0]);
 
-            fprintf(stderr, "Will execute command %s\n",argv2[0]);
-            fprintf(stderr, "Will fork command %s\n", argv2[0]);
+            // sigaction(SIGINT, NULL, &oldact);   // the  previous action for SIGINT is saved in oldact
+            // newact = oldact;
 
-            sigaction(SIGINT, NULL, &oldact);   // the  previous action for SIGINT is saved in oldact
-            newact = oldact;
+            // if ((pid = fork()) < 0) {           // fork error, i.e. too many processes
+            //     error(0, errno, "fork error\n"); // will fprintf the error and go on
+            // }
 
-            if ((pid = fork()) < 0) {           // fork error, i.e. too many processes
-                error(0, errno, "fork error\n"); // will fprintf the error and go on
-            }
-
-            else if (pid == 0) {                 // child process
+            // else if (pid == 0) {                 // child process
                 
-                // ============== NEW CODE HERE ==============
-                newact.sa_handler = SIG_DFL;
-                sigaction(SIGINT, &newact, NULL);   // reset SIGINT default for child
+            //     // ============== NEW CODE HERE ==============
+            //     newact.sa_handler = SIG_DFL;
+            //     sigaction(SIGINT, &newact, NULL);   // reset SIGINT default for child
 
-                //execlp("sleep", "sleep", "5", NULL);    // if successful, child will go on with new executable
-                execvp(argv2[0] , &argv2[0]);
-                error(EXIT_FAILURE, errno, "execvp error\n"); // if exec not successful, just exit child
-            }
+            //     //execlp("sleep", "sleep", "5", NULL);    // if successful, child will go on with new executable
+            //     execvp(argv2[0] , &argv2[0]);
+            //     error(EXIT_FAILURE, errno, "execvp error\n"); // if exec not successful, just exit child
+            // }
 
-            else {                               // pid > 0: parent (shell) process
+            // else {                               // pid > 0: parent (shell) process
 
-                // ============== NEW CODE HERE ==============
-                newact.sa_handler = SIG_IGN;
-                sigaction(SIGINT, &newact, NULL);   // ignore SIGINT while waiting
+            //     // ============== NEW CODE HERE ==============
+            //     newact.sa_handler = SIG_IGN;
+            //     sigaction(SIGINT, &newact, NULL);   // ignore SIGINT while waiting
 
-                waitpid(pid, &wait_status, 0);
+            //     waitpid(pid, &wait_status, 0);
 
-                // ============== NEW CODE HERE ==============
-                sigaction(SIGINT, &oldact, NULL);   // restore SIGINT when child finishes
+            //     // ============== NEW CODE HERE ==============
+            //     sigaction(SIGINT, &oldact, NULL);   // restore SIGINT when child finishes
 
-                fprintf(stderr, "Ended child process %s\n",argv2[0]);
+            //     fprintf(stderr, "Ended child process %s\n",argv2[0]);
 
                 // do something with wait_status
-            }
+            //}
         }
     }
 

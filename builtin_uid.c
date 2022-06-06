992825f5caa0355c87c5dc20f5f1d2f4e926bb88
timofabregat <80836792+timofabregat@users.noreply.github.com>
2022-06-06 16:44:43 -0300

    uid

--- builtin_uid.c
+++ builtin_uid.c
@@ -1,5 +1,17 @@
 #include "minish.h"
+#include <pwd.h>
 
 int builtin_uid (int argc, char ** argv){
-    return 0;
+
+    if(argc == 1){
+        struct passwd *pws;
+        int id = geteuid();
+        pws = getpwuid(id);
+        printf("User ID: %d\nUsername: %s\n",id, pws->pw_name);
+        return 0;
+    }
+
+    else{
+        return -1; //Error
+    }
 };

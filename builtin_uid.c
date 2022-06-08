f984479e3152742317df3f5233e7decfc6ae165f
timofabregat <timofabregat@gmail.com>
2022-06-08 11:22:42 -0300

    Se agrega comentarios y errores en algunas funciones

--- builtin_uid.c
+++ builtin_uid.c
@@ -1,6 +1,9 @@
 #include "minish.h"
 
 int builtin_uid (int argc, char ** argv){
+
+    /*Se busca el id de usuario, se encuentra el password asociado al id
+     y a su vez el nombre de usuario asociado al password*/
     if(argc == 1){
         struct passwd *pws;
         int id = geteuid();
@@ -8,7 +11,11 @@ int builtin_uid (int argc, char ** argv){
         printf("User ID: %d\nUsername: %s\n",id,pws->pw_name);
         return 0;
     }
+
+    //Built-in no soporta mas de un argumento
     else{
-        return -1; //Completar Error
+        printf("\033[1;31m");
+        error(EXIT_SUCCESS,0,"\033[31mDemasiados Argumentos\033[0m");
+        return -1; 
     }
 };

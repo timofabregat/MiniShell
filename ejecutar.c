91bc20be52ed541a27c6660ea222f0b592151791
timofabregat <timofabregat@gmail.com>
2022-06-08 09:13:39 -0300

    se prueba error en help, se emprolija texto help

--- ejecutar.c
+++ ejecutar.c
@@ -1,17 +1,17 @@
 #include "minish.h"
 
-#define HELP_CD      "cd [..|dir] - cambia de directorio corriente"
-#define HELP_DIR     "dir [str]- muestra archivos en directorio corriente, que tengan 'str'"
-#define HELP_EXIT    "exit [status] - finaliza el minish con un status de retorno (por defecto 0)"
-#define HELP_HELP    "help [cd|dir|exit|help|history|getenv|pid|setenv|status|uid]"
-#define HELP_HISTORY "history [N] - muestra los últimos N (10) comandos escritos"
-#define HELP_GETENV  "getenv var [var] - muestra valor de variable(s) de ambiente"
-#define HELP_PID     "pid - muestra Process Id del minish"
-#define HELP_SETENV  "setenv var valor - agrega o cambia valor de variable de ambiente"
-#define HELP_STATUS  "status - muestra status de retorno de ultimo comando ejecutado"
-#define HELP_UID     "uid - muestra nombre y número de usuario dueño del minish"
-#define HELP_GID     "gid -  muestra el grupo principal y los grupos secundarios del usuario."     //completar
-#define HELP_UNSETENV "unsetenv var [var...] - elimina variables de ambiente."       //completar 
+#define HELP_CD      "cd [..|dir]            - cambia de directorio corriente"
+#define HELP_DIR     "dir [str]              - muestra archivos en directorio corriente, que tengan 'str'"
+#define HELP_EXIT    "exit [status]          - finaliza el minish con un status de retorno (por defecto 0)"
+#define HELP_HELP    "help [built-in]        - Muestra el pantalla la funcionalidad del built-in sleccionado"
+#define HELP_HISTORY "history [N]            - muestra los últimos N (10) comandos escritos"
+#define HELP_GETENV  "getenv var [var]       - muestra valor de variable(s) de ambiente"
+#define HELP_PID     "pid                    - muestra Process Id del minish"
+#define HELP_SETENV  "setenv var valor       - agrega o cambia valor de variable de ambiente"
+#define HELP_STATUS  "status                 - muestra status de retorno de ultimo comando ejecutado"
+#define HELP_UID     "uid                    - muestra nombre y número de usuario dueño del minish"
+#define HELP_GID     "gid                    - muestra el grupo principal y los grupos secundarios del usuario."     //completar
+#define HELP_UNSETENV "unsetenv var [var...]  - elimina variables de ambiente."       //completar 
 
 struct builtin_struct builtin_arr[] = {
     { "cd", builtin_cd, HELP_CD },
